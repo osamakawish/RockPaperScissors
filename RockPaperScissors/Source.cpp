109835aeb05a5968d3f68e845dfdeb9c66c0f98c
Osama Kawish <osamakawish@gmail.com>
2017-12-14 20:19:04 -0500

    ! A working program that takes user input, and repeatedly prompts until user quits.

--- RockPaperScissors/Source.cpp
+++ RockPaperScissors/Source.cpp
@@ -1,65 +1,64 @@
 #include <iostream>
 #include <random>
-#include <sstream>
-#include <cctype>
-#include <vector>
+#include <map>
+#include <iterator>
 #include <locale>	// Convert characters to uppercase.
 using namespace std;
 
 const string QUIT = "QUIT";
 const enum rpsChoice { Rock, Paper, Scissors };
-const vector<rpsChoice> rpsCompare = { Rock, Paper, Scissors };
+const map<string, rpsChoice> stringRPS = { { "ROCK", Rock } , { "PAPER" , Paper } , {"SCISSORS", Scissors } };
 
-// Prompts a user to choose from rock, papers, or scissors;
-void prompt(string &input) {
-	// Ask for user to pick from rock, papers, scissors.
-	cout << "Pick rock, paper, or scissor. (Type your choice)" << endl;
+bool contains(map<string, rpsChoice> M, string x) {
+	auto iterate = M.begin(); 
+	auto toFind = M.find(x);
 
-	// Add that QUIT needs to be typed to quit game.
-	cout << "Type 'QUIT' to quit game." << endl;
-	getline(cin, input); cout << endl << endl;
+	while (iterate != M.end()) {
+		if (iterate == toFind) { return true; }
+		iterate++;
+	}
+
+	return false;
 }
-
 
 // Converts a string to uppercase.
 string toUpper(string s) {
 	// Initiate the string to be returned.
 	string toReturn = ""; 
 	
-	for each (char ch in s)
-	{
-		// Convert each character to uppercase.
-		toReturn += toupper(ch);
-	}
+	// Convert each character to uppercase.
+	for each (char ch in s) { toReturn += toupper(ch); }
 	
 	return toReturn;
 }
 
-// Converts a string to an RPS choice.
-rpsChoice toRPS(string S) {
-	if (S == "ROCK") return Rock;
-	if (S == "PAPER") return Paper;
-	if (S == "SCISSORS") return Scissors;
+// Prompts a user to choose from rock, papers, or scissors.
+// Then updates input to user's value.
+void prompt(string &input, string &Input) {
+	// Ask for user to pick from rock, papers, scissors.
+	cout << "Pick rock, paper, or scissor. (Type your choice)" << endl;
 
-	else
-	{
-		throw exception("S is not an RPS choice.");
-	}
+	// Add that QUIT needs to be typed to quit game.
+	cout << "Type 'QUIT' to quit game." << endl;
+	getline(cin, input); cout << endl << endl;
+
+	// Update Input
+	Input = toUpper(input);
 }
 
 int main() {
-	string input;
-	prompt(input);
+	// input: The input provided by the user.
+	// Input: An all uppercase string of input, to make the game case insensitive.
+	string input; string Input; 
+	prompt(input, Input); Input = toUpper(input);
 
 	// Repeat until input == QUIT.
-	while (toUpper(input) != QUIT) {
+	while (Input != QUIT || contains(stringRPS, Input)) {
+		prompt(input, Input); Input = toUpper(input);
+
+		// Get computer to select from RPS
+
+		// Update wins, losses, and draws.
 
 	}
-	
-	// Repeat Q if user does not pick one of three.
-
-	// Get computer to select from RPS
-
-	// Update wins, losses, and draws.
-
 }
